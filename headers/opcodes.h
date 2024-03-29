42e3bb6b8a457ec2f9552f6a15d7761f46a587c3
AleBitCode <146528926+AleBitCode@users.noreply.github.com>
2024-03-29 21:17:06 +0100

    altri prototipi di opcodes

--- headers/opcodes.h
+++ headers/opcodes.h
@@ -58,53 +58,170 @@ class Opcodes {
 		void DEC_DE(LR35902&);
 		void INC_E(LR35902&);
 		void DEC_E(LR35902&);
-		void LD_E_BYTEDATA(LR35902&);
+		void LD_E_BYTEDATA(LR35902&, Byte);
 		void RRA(LR35902&);
-		void JR_
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-		void
-
+		void JR_NZ_BYTEDATA(LR35902&, Byte);
+		void LD_HL_WORDDATA(LR35902&, Word);
+		void LD_ADDRESSOFH_A_INC_H(LR35902&);
+		void INC_HL(LR35902&);
+		void INC_H(LR35902&);
+		void DEC_H(LR35902&);
+		void LD_H_BYTEDATA(LR35902&, Byte);
+		void DAA(LR35902&);
+		void JR_Z_BYTEDATA(LR35902&, Byte);
+		void ADD_HL_HL(LR35902&);
+		void LD_A_ADDRESSOFHL_INC_HL(LR35902&);
+		void DEC_HL(LR35902&);
+		void INC_L(LR35902&);
+		void DEC_L(LR35902&);
+		void LD_L_BYTEDATA(LR35902&);
+		void CPL(LR35902&);				//complement, esegue il not logico sui bit del registro A
+		void JR_NC_BYTEDATA(LR35902&, Byte);
+		void LD_SP_WORDDATA(LR35902&, Word);
+		void LD_ADDRESSOFHL_A_DEC_HL(LR35902&);
+		void INC_SP(LR35902&);
+		void INC_ADDRESSOFHL(LR35902&);
+		void DEC_ADDRESSOFHL(LR35902&);
+		void LD_ADDRESSOFHL_BYTEDATA(LR35902&, Byte);
+		void SCF(LR35902&); 		//DA CONTROLLARE
+		void JR_C_BYTEDATA(LR35902&, Byte);
+		void ADD_HL_SP(LR35902&);
+		void LD_A_ADDRESSOFHL_DEC_HL(LR35902&);
+		void DEC_SP(LR35902&);
+		void INC_A(LR35902&);
+		void DEC_A(LR35902&);
+		void LD_A_BYTEDATA(LR35902&, Byte);
+		void CCF(LR35902&); 		//DA CONTROLLARE
+		void LD_B_B(LR35902&);
+		void LD_B_C(LR35902&);
+		void LD_B_D(LR35902&);
+		void LD_B_E(LR35902&);
+		void LD_B_H(LR35902&);
+		void LD_B_L(LR35902&);
+		void LD_B_ADDRESSOFHL(LR35902&);
+		void LD_B_A(LR35902&);
+		void LD_C_B(LR35902&);
+		void LD_C_C(LR35902&);
+		void LD_C_D(LR35902&);
+		void LD_C_E(LR35902&);
+		void LD_C_H(LR35902&);
+		void LD_C_L(LR35902&);
+		void LD_C_ADDRESSOFHL(LR35902&);
+		void LD_C_A(LR35902&);
+		void LD_D_B(LR35902&);
+		void LD_D_C(LR35902&);
+		void LD_D_D(LR35902&);
+		void LD_D_E(LR35902&);
+		void LD_D_H(LR35902&);
+		void LD_D_L(LR35902&);
+		void LD_D_ADDRESSOFHL(LR35902&);
+		void LD_D_A(LR35902&);
+		void LD_E_B(LR35902&);
+		void LD_E_C(LR35902&);
+		void LD_E_D(LR35902&);
+		void LD_E_E(LR35902&);
+		void LD_E_H(LR35902&);
+		void LD_E_L(LR35902&);
+		void LD_E_ADDRESSOFHL(LR35902&);
+		void LD_E_A(LR35902&);
+		void LD_H_B(LR35902&);
+		void LD_H_C(LR35902&);
+		void LD_H_D(LR35902&);
+		void LD_H_E(LR35902&);
+		void LD_H_H(LR35902&);
+		void LD_H_L(LR35902&);
+		void LD_H_ADDRESSOFHL(LR35902&);
+		void LD_H_A(LR35902&);
+		void LD_L_B(LR35902&);
+		void LD_L_C(LR35902&);
+		void LD_L_D(LR35902&);
+		void LD_L_E(LR35902&);
+		void LD_L_H(LR35902&);
+		void LD_L_L(LR35902&);
+		void LD_L_ADDRESSOFHL(LR35902&);
+		void LD_L_A(LR35902&);
+		void LD_ADDRESSOFHL_B(LR35902&);
+		void LD_ADDRESSOFHL_C(LR35902&);
+		void LD_ADDRESSOFHL_D(LR35902&);
+		void LD_ADDRESSOFHL_E(LR35902&);
+		void LD_ADDRESSOFHL_H(LR35902&);
+		void LD_ADDRESSOFHL_L(LR35902&);
+		void HALT(LR35902&); 		//DA CONTROLLARE
+		void LD_ADDRESSOFHL_A(LR35902&);
+		void LD_A_B(LR35902&);
+		void LD_A_C(LR35902&);
+		void LD_A_D(LR35902&);
+		void LD_A_E(LR35902&);
+		void LD_A_H(LR35902&);
+		void LD_A_L(LR35902&);
+		void LD_A_ADDRESSOFHL(LR35902&);
+		void LD_A_A(LR35902&);
+		void ADD_A_B(LR35902&);
+		void ADD_A_C(LR35902&);
+		void ADD_A_D(LR35902&);
+		void ADD_A_E(LR35902&);
+		void ADD_A_H(LR35902&);
+		void ADD_A_L(LR35902&);
+		void ADD_A_ADDRESSOFHL(LR35902&);
+		void ADD_A_A(LR35902&);
+		void ADC_A_B(LR35902&);
+		void ADC_A_C(LR35902&);
+		void ADC_A_D(LR35902&);
+		void ADC_A_E(LR35902&);
+		void ADC_A_H(LR35902&);
+		void ADC_A_L(LR35902&);
+		void ADC_A_ADDRESSOFHL(LR35902&);
+		void ADC_A_A(LR35902&);
+		void SUB_B(LR35902&);
+		void SUB_C(LR35902&);
+		void SUB_D(LR35902&);
+		void SUB_E(LR35902&);
+		void SUB_H(LR35902&);
+		void SUB_L(LR35902&);
+		void SUB_ADDRESSOFHL(LR35902&);
+		void SUB_A(LR35902&);
+		void SBC_A_B(LR35902&);
+		void SBC_A_C(LR35902&);
+		void SBC_A_D(LR35902&);
+		void SBC_A_E(LR35902&);
+		void SBC_A_H(LR35902&);
+		void SBC_A_L(LR35902&);
+		void SBC_A_ADDRESSOFHL(LR35902&);
+		void SBC_A_A(LR35902&);
+		void AND_B(LR35902&);
+		void AND_C(LR35902&);
+		void AND_D(LR35902&);
+		void AND_E(LR35902&);
+		void AND_H(LR35902&);
+		void AND_L(LR35902&);
+		void AND_ADDRESSOFHL(LR35902&);
+		void AND_A(LR35902&);
+		void XOR_B(LR35902&);
+		void XOR_C(LR35902&);
+		void XOR_D(LR35902&);
+		void XOR_E(LR35902&);
+		void XOR_H(LR35902&);
+		void XOR_L(LR35902&);
+		void XOR_ADDRESSOFHL(LR35902&);
+		void XOR_A(LR35902&);
+		void OR_B(LR35902&);
+		void OR_C(LR35902&);
+		void OR_D(LR35902&);
+		void OR_E(LR35902&);
+		void OR_H(LR35902&);
+		void OR_L(LR35902&);
+		void OR_ADDRESSOFHL(LR35902&);
+		void OR_A(LR35902&);
+		void CP_B(LR35902&);
+		void CP_C(LR35902&);
+		void CP_D(LR35902&);
+		void CP_E(LR35902&);
+		void CP_H(LR35902&);
+		void CP_L(LR35902&);
+		void CP_ADDRESSOFHL(LR35902&);
+		void CP_A(LR35902&);
+		void RET_NZ(LR35902&);
+		void POP_BC(LR35902&);
 };
 
 
