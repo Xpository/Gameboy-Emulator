68d75516e5c5ce63215b0aaf089dc205da1a3268
RehTrizZ <95867610+RehTrizZ@users.noreply.github.com>
2024-04-13 17:58:00 +0000

    Commit di prova, aggiunte pochissime cose

--- headers/graphics.h
+++ headers/graphics.h
@@ -1,9 +1,16 @@
+#pragma once 
+
 #include "types.h"
 
 using namespace nstdtypes;
 
 class PPU{
     public:
-        
+        PPU();
     private:
+        Byte screen[160][144];
+
+        // 1024 Byte
+        
+
 };
--- headers/types.h
+++ headers/types.h
@@ -3,4 +3,5 @@ namespace nstdtypes {
 	using Word = unsigned short; // 16 bit
 	using B32 = unsigned long; // 32 bit
 	using B64 = unsigned long long; // 64 bit
+	
 }
